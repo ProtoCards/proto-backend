a35f8d56cf5f7b17b519a837500f90eecdb38212
Devin Miller <devzmiller@gmail.com>
2018-02-14 20:23:22 -0800

    Add dependencies to package.json and add app.js with some boilerplate.

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+
+module.exports = app
