8c335ae1c78082d2655eb663c9028b10b86ab4c0
Devin Miller <devzmiller@gmail.com>
2018-02-15 15:44:18 -0800

    Add mongo and dotenv to package.json. Add some stuff to app.js so server starts. Add gitignore.

--- app.js
+++ app.js
@@ -7,4 +7,11 @@ const morgan = require('morgan')
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 
+app.get('/', (req, res) => {
+  res.send("You made it!")
+})
+
+const listener = () => console.log(`Listening on port ${port}`);
+app.listen(port, listener)
+
 module.exports = app
