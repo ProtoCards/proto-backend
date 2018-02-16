1d6bda1d2575683a515c3e301fee724e11a190d2
Devin Miller <devzmiller@gmail.com>
2018-02-15 16:32:33 -0800

    Add express-mongo-db to make it easy to connect to db. Create a functional /projects route to test.

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const expressMongo = require('express-mongo-db')
+require('dotenv').config();
 
+const app = express()
+const port = process.env.PORT || 3000
+
+
+app.use(expressMongo(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds023550.mlab.com:23550/protocards`))
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
   res.send("You made it!")
+})
+
+app.get('/projects', (req, res) => {
+  req.db.collection('projects').find().toArray((err, results) => {
+    res.send(results)
+  })
 })
 
 const listener = () => console.log(`Listening on port ${port}`);
