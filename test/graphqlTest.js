a47501bdd5803a0452e8bd2f1636089eb3613c32
Devin Miller <devzmiller@gmail.com>
2018-02-28 16:26:08 -0800

    Get rid of defunct test. One test failing, not sure why.

--- test/graphqlTest.js
+++ test/graphqlTest.js
@@ -1,23 +1,12 @@
 const graphql = require('graphql');
 const chai = require('chai');
 const chaiHttp = require('chai-http')
-const gqlRoutes = require('../src/routes/graphql')
 const mongo = require('../mongoConfig')
 const app = require('../app')
 chai.use(chaiHttp)
 const expect = chai.expect;
 
 describe('Project routes', () => {
-
-  it('does stuff', (done) => {
-    chai.request(app)
-      .get('/graphql')
-      .send({"query": "{ hello }"})
-      .end((err, res) => {
-        expect(res.body.data.hello).to.equal('Hello World')
-        done()
-      })
-  })
 
   it('returns an array of projects', (done) => {
     setTimeout(done, 3000)
