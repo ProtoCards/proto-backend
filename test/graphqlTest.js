509461fd8cd6409dfd5af6cc0747392919058aa6
Devin Miller <devzmiller@gmail.com>
2018-03-01 20:24:21 -0800

    Progress test beforeEach--not working yet

--- test/graphqlTest.js
+++ test/graphqlTest.js
@@ -2,11 +2,26 @@ const graphql = require('graphql');
 const chai = require('chai');
 const chaiHttp = require('chai-http')
 const mongo = require('../mongoConfig')
+const db = require('../mongoConfig').getDB
 const app = require('../app')
 chai.use(chaiHttp)
 const expect = chai.expect;
 
 describe('Project routes', () => {
+  beforeEach(() => {
+    mongo.connectDB((err) => {
+      if (err) console.log(err)
+      const projectId = db().collection('projects').insertOne({name: "a project", ownerId: 1})
+      .then((project) => {
+        return project.ops[0]._id
+      })
+      console.log(projectId)
+      // mongo.db().collection('cards').insertOne({projectId: projectId, quantity: 2, properties: [{name: "title", fieldId: "1", content: "A Card Title"}]})
+      //   .then((card)) => {
+      //     return
+      //   }
+    })
+  })
 
   it('returns an array of projects', (done) => {
     setTimeout(done, 3000)
