10b8664a3f67523d04e088aa434c6db3b42f18a0
Devin Miller <devzmiller@gmail.com>
2018-03-05 16:55:56 -0800

    Fix connection to test database

--- mongoConfig.js
+++ mongoConfig.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient
 let env = process.env.NODE_ENV
 let uri
+let dbName
 if (env === 'dev') {
   uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds023550.mlab.com:23550/protocards`
+  dbName = 'protocards'
 } else if (env === 'test') {
   uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds119486.mlab.com:19486/protocards-test`
+  dbName = 'protocards-test'
 }
 
 let _db
@@ -13,7 +16,7 @@ let _db
 const connectDB = async (callback) => {
   try {
     MongoClient.connect(uri, (err, client) => {
-      _db = client.db('protocards')
+      _db = client.db(dbName)
       return callback(err)
     })
   } catch (e) {
