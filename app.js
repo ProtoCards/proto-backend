326d0881878791653e360eb80cac095a3491d108
Devin Miller <devzmiller@gmail.com>
2018-03-10 14:36:28 -0800

    change port to 3001

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const graphqlHTTP = require('express-graphql');
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.disable('x-powered-by')
 app.use(bodyParser.json())
